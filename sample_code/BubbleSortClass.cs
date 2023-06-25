c3d33bf2b7ed631a324af1ce985c5b5cbf0c9f23
김현승 <corn120033@gmail.com>
2023-06-25 23:48:23 +0900

    4.1.1. 버블 정렬 코드 주석 추가

--- sample_code/BubbleSortClass.cs
+++ sample_code/BubbleSortClass.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class BubbleSortClass
 {
+  // 버블 정렬 함수
   public static void BubbleSort(int[] arr)
   {
+    // 정렬 횟수, 정렬 범위, 데이터 임시 저장 변수
     int i, j, temp;
+    // n 크기 선언
     int len = arr.Length;
+
+    // n번 반복
     for (i = 0; i < len - 1; i++)
     {
+      // n번 반복할 때마다 반복 범위 줄여나감
       for (j = 0; j < len - i - 1; j++)
       {
+        // 현재 원소가 다음 원소보다 클 경우 실행
         if (arr[j] > arr[j + 1])
         {
+          // 현재 원소와 다음 원소 위치 변경
           temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
